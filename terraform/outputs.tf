e4c787034b648ee35529e9fb5eeb1ede424a1a27
Maurício Fedatto <mfedatto@gmail.com>
2025-01-26 12:22:45 -0300

    Add output for RabbitMQ network name in Terraform configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "vpc_network_name" {
   description = "The name of the Docker network"
   value       = var.vpc_network_name
 }
+output "rabbitmq_network_name" {
+  description = "The name of the Docker network"
+  value       = var.rabbitmq_network_name
+}
 
 output "docker_host" {
   description = "The Docker host"
