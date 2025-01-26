a7c766547fff65a4f781244be7b0a078dd1fd685
Maurício Fedatto <mfedatto@gmail.com>
2025-01-26 12:53:53 -0300

    Create RabbitMQ data directory before volume creation in Terraform

--- terraform/volumes.tf
+++ terraform/volumes.tf
@@ -1,10 +1,15 @@
-data "external" "check_rabbitmq-data_volume" {
-  program = ["bash", "-c", "docker volume inspect ${var.rabbitmq-data_volume_name} >/dev/null 2>&1 && echo '{\"exists\": \"true\"}' || echo '{\"exists\": \"false\"}'"]
+resource "null_resource" "create_rabbitmq_data_directory" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${local.rabbitmq_data_volume_path}"
+  }
 }
 
 resource "null_resource" "create_rabbitmq_data_volume" {
   provisioner "local-exec" {
     command = "docker volume create --name ${var.rabbitmq-data_volume_name} --opt type=none --opt device=${local.rabbitmq_data_volume_path} --opt o=bind"
   }
-  depends_on = [data.external.check_rabbitmq-data_volume]
+  depends_on = [
+    data.external.check_rabbitmq-data_volume,
+    null_resource.create_rabbitmq_data_directory
+  ]
 }
