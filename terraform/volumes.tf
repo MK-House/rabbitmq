61549051901c6d3500aa3e355327e8569f2c672d
Maurício Fedatto <mfedatto@gmail.com>
2025-01-26 12:54:39 -0300

    Add external data source to check existence of RabbitMQ data volume

--- terraform/volumes.tf
+++ terraform/volumes.tf
@@ -1,3 +1,7 @@
+data "external" "check_rabbitmq-data_volume" {
+  program = ["bash", "-c", "docker volume inspect ${var.rabbitmq-data_volume_name} >/dev/null 2>&1 && echo '{\"exists\": \"true\"}' || echo '{\"exists\": \"false\"}'"]
+}
+
 resource "null_resource" "create_rabbitmq_data_directory" {
   provisioner "local-exec" {
     command = "mkdir -p ${local.rabbitmq_data_volume_path}"
